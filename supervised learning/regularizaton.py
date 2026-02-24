02ef5ca723be8e03af3ccd353ba1c285e7976510
Kami <josephkiarie561@gmail.com>
2026-02-24 12:11:11 +0300

    elasatic net

--- supervised learning/regularizaton.py
+++ supervised learning/regularizaton.py
@@ -25,11 +25,16 @@ class RidgeRegularization():
         return self.alpha * w
 
 class ElasticNet():
-    def __init__(self):
-        pass
+    def __init__(self, alpha, l1_ratio=0.5):
+        self.alpha = alpha
+        self.l1_ratio = l1_ratio
 
-    def __call__(self, *args, **kwds):
-        pass
+    def __call__(self, w):
+        l1_contr = self.l1_ratio * np.linalg.norm(w)
+        l2_contr = (1 - self.l1_ratio) * 0.5 * w.T.dot(w)
+        return self.alpha * (l1_contr + l2_contr)
 
     def grad(self, w):
-        pass
+        l1_contr = self.l1_ratio * np.sign(w)
+        l2_contr = (1 - self.l1_ratio) * w
+        return self.alpha * (l1_contr + l2_contr)
