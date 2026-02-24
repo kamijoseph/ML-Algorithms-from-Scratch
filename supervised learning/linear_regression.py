c32afb9abd0f46d0ea16f367d0d3a447528d046e
Kami <josephkiarie561@gmail.com>
2026-02-24 11:51:40 +0300

    regressors skeleton

--- supervised learning/linear_regression.py
+++ supervised learning/linear_regression.py
@@ -1,2 +1,71 @@
 
 # linear regularization
+class Regressor(object):
+    def __init__(self):
+        pass
+
+    def initialize_weights(self, n_features):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+class LinearRegression(Regressor):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def fit(self, X, y):
+        pass
+
+class LassoRegression(Regressor):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+class PolynomialRegression(Regressor):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+class RidgeRegression(Regressor):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class PolynomialRidgeRegression(Regressor):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+class ElasticNet(Regressor):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
